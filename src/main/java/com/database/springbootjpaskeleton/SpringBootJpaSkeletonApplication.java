40046f58b0bd34ad2a21a96c4dc38e019d343160
yakup.kariksiz <yakup.kariksiz@epias.com.tr>
2021-05-14 07:10:23 +0300

    feat: implement insert and update jpa methods

--- src/main/java/com/database/springbootjpaskeleton/SpringBootJpaSkeletonApplication.java
+++ src/main/java/com/database/springbootjpaskeleton/SpringBootJpaSkeletonApplication.java
@@ -27,13 +27,14 @@ public class SpringBootJpaSkeletonApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         logger.info("User id 10001 -> {}", repository.findById(10001));
+        logger.info("Inserting -> {}", repository.insert(new Person("ERDEM", "ISTANBUL", new Date())));
+        logger.info("Updating 10004 -> {}", repository.update(new Person(10004, "İBRAHİM ERDEM", "KADIKÖY", new Date())));
         /*
         logger.info("All users -> {}", repository.findAll());
         logger.info("User name SWEN -> {}", repository.findByName("GUILLERMO"));
         logger.info("Deleting 10003 -> No of Rows Deleted - {}", repository.deleteById(10003));
         logger.info("Deleting 10003 -> No of Rows Deleted - {}", repository.deleteAll());
-        logger.info("Inserting 10005 -> {}", repository.insert(new Person(10005, "ERDEM", "ISTANBUL", new Date())));
-        logger.info("Updating 10005 -> {}", repository.update(new Person(10005, "İBRAHİM ERDEM", "KADIKÖY", new Date())));
+
          */
     }
 }
