ce24443f76bf03fa681e198d8b89f5d7c54f992a
yakup.kariksiz <yakup.kariksiz@epias.com.tr>
2021-05-14 06:25:25 +0300

    feat: define a person entity

--- src/main/java/com/database/springbootjpaskeleton/jdbc/entity/Person.java
+++ src/main/java/com/database/springbootjpaskeleton/jdbc/entity/Person.java
@@ -1,10 +1,17 @@
 package com.database.springbootjpaskeleton.jdbc.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Person {
 
+    @Id
+    @GeneratedValue
     private int id;
+
     private String name;
     private String location;
     private Date birthDate;
@@ -14,6 +21,12 @@ public class Person {
 
     public Person(int id, String name, String location, Date birthDate) {
         this.id = id;
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
+
+    public Person(String name, String location, Date birthDate) {
         this.name = name;
         this.location = location;
         this.birthDate = birthDate;
